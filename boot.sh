a4a2a1eafef5eefed8e9f330823c4997025fd7fa
Coby Powers <mail@cobypowers.com>
2025-08-04 23:30:21 -0500

    feat: change logos, update install and boot scripts

--- boot.sh
+++ boot.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
-ansi_art='                 ▄▄▄                                                   
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
-███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
-███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
-                                       ███   █▀                                  '
+ANSI_ART='   ▄████████ ███▄▄▄▄      ▄████████    ▄████████  ▄████████    ▄█    █▄    ▄██   ▄   
+  ███    ███ ███▀▀▀██▄   ███    ███   ███    ███ ███    ███   ███    ███   ███   ██▄ 
+  ███    ███ ███   ███   ███    ███   ███    ███ ███    █▀    ███    ███   ███▄▄▄███ 
+  ███    ███ ███   ███   ███    ███  ▄███▄▄▄▄██▀ ███         ▄███▄▄▄▄███▄▄ ▀▀▀▀▀▀███ 
+▀███████████ ███   ███ ▀███████████ ▀▀███▀▀▀▀▀   ███        ▀▀███▀▀▀▀███▀  ▄██   ███ 
+  ███    ███ ███   ███   ███    ███ ▀███████████ ███    █▄    ███    ███   ███   ███ 
+  ███    ███ ███   ███   ███    ███   ███    ███ ███    ███   ███    ███   ███   ███ 
+  ███    █▀   ▀█   █▀    ███    █▀    ███    ███ ████████▀    ███    █▀     ▀█████▀  
+                                      ███    ███                                     '
 
 clear
 echo -e "\n$ansi_art\n"
 
+if [[ ! -f /etc/arch-release ]]; then
+  echo "You must be using an Arch-based distro to install and use Anarchy."
+  exit 1
+fi
+
 sudo pacman -Sy --noconfirm --needed git
 
-# Use custom repo if specified, otherwise default to basecamp/omarchy
-OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
+# Use custom repo if specified, otherwise default to CobyPowers/anarchy
+ANARCHY_REPO="${ANARCHY_REPO:-CobyPowers/anarchy}"
 
-echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
-rm -rf ~/.local/share/omarchy/
-git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
+echo -e "\nCloning Anarchy from: https://github.com/${ANARCHY_REPO}.git"
+rm -rf ~/.local/share/anarchy/
+git clone "https://github.com/${ANARCHY_REPO}.git" ~/.local/share/anarchy >/dev/null
 
 # Use custom branch if instructed
-if [[ -n "$OMARCHY_REF" ]]; then
-  echo -e "\eUsing branch: $OMARCHY_REF"
-  cd ~/.local/share/omarchy
-  git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
+if [[ -n "$ANARCHY_REF" ]]; then
+  echo -e "\eUsing branch: $ANARCHY_REF"
+  cd ~/.local/share/anarchy
+  git fetch origin "${ANARCHY_REF}" && git checkout "${ANARCHY_REF}"
   cd -
 fi
 
 echo -e "\nInstallation starting..."
-source ~/.local/share/omarchy/install.sh
+source ~/.local/share/anarchy/install.sh
--- install.sh
+++ install.sh
@@ -3,20 +3,18 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-OMARCHY_INSTALL=~/.local/share/omarchy/install
+ANARCHY_INSTALL=~/.local/share/anarchy/install
 
 # Give people a chance to retry running the installation
 catch_errors() {
-  echo -e "\n\e[31mOmarchy installation failed!\e[0m"
-  echo "You can retry by running: bash ~/.local/share/omarchy/install.sh"
-  echo "Get help from the community: https://discord.gg/tXFUdasqhY"
+  echo -e "\n\e[31mAnarchy installation failed!\e[0m"
+  echo "You can retry by running: bash ~/.local/share/anarchy/install.sh"
 }
-
 trap catch_errors ERR
 
 show_logo() {
   clear
-  tte -i ~/.local/share/omarchy/logo.txt --frame-rate ${2:-120} ${1:-expand}
+  tte -i ~/.local/share/anarchy/logo.txt --frame-rate ${2:-120} ${1:-expand}
   echo
 }
 
@@ -25,60 +23,43 @@ show_subtext() {
   echo
 }
 
+load_shell_scripts() {
+  for script in "$1/*.sh"; do
+    source "$script"
+  done  
+}
+
 # Install prerequisites
-source $OMARCHY_INSTALL/preflight/aur.sh
-source $OMARCHY_INSTALL/preflight/presentation.sh
-source $OMARCHY_INSTALL/preflight/migrations.sh
+load_shell_scripts $ANARCHY_INSTALL/preflight
 
-# Configuration
+# Install config files
 show_logo beams 240
-show_subtext "Let's install Omarchy! [1/5]"
-source $OMARCHY_INSTALL/config/identification.sh
-source $OMARCHY_INSTALL/config/config.sh
-source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
-source $OMARCHY_INSTALL/config/fix-fkeys.sh
-source $OMARCHY_INSTALL/config/network.sh
-source $OMARCHY_INSTALL/config/power.sh
-source $OMARCHY_INSTALL/config/timezones.sh
-source $OMARCHY_INSTALL/config/login.sh
-source $OMARCHY_INSTALL/config/nvidia.sh
+show_subtext "Installing config files [1/5]"
+load_shell_scripts $ANARCHY_INSTALL/config
 
-# Development
+# Install development tools
 show_logo decrypt 920
 show_subtext "Installing terminal tools [2/5]"
-source $OMARCHY_INSTALL/development/terminal.sh
-source $OMARCHY_INSTALL/development/development.sh
-source $OMARCHY_INSTALL/development/nvim.sh
-source $OMARCHY_INSTALL/development/ruby.sh
-source $OMARCHY_INSTALL/development/docker.sh
-source $OMARCHY_INSTALL/development/firewall.sh
+load_shell_scripts $ANARCHY_INSTALL/development
 
-# Desktop
+# Install desktop tools
 show_logo slice 60
 show_subtext "Installing desktop tools [3/5]"
-source $OMARCHY_INSTALL/desktop/desktop.sh
-source $OMARCHY_INSTALL/desktop/hyprlandia.sh
-source $OMARCHY_INSTALL/desktop/theme.sh
-source $OMARCHY_INSTALL/desktop/bluetooth.sh
-source $OMARCHY_INSTALL/desktop/asdcontrol.sh
-source $OMARCHY_INSTALL/desktop/fonts.sh
-source $OMARCHY_INSTALL/desktop/printer.sh
+load_shell_scripts $ANARCHY_INSTALL/desktop
 
-# Apps
+# Install default applications
 show_logo expand
 show_subtext "Installing default applications [4/5]"
-source $OMARCHY_INSTALL/apps/webapps.sh
-source $OMARCHY_INSTALL/apps/xtras.sh
-source $OMARCHY_INSTALL/apps/mimetypes.sh
+load_shell_scripts $ANARCHY_INSTALL/apps
 
-# Updates
+# Update system packages
 show_logo highlight
 show_subtext "Updating system packages [5/5]"
 sudo updatedb
 sudo pacman -Syu --noconfirm
 
-# Reboot
+# Reboot system to apply changes
 show_logo laseretch 920
 show_subtext "You're done! So we'll be rebooting now..."
-sleep 2
+sleep 3
 reboot
